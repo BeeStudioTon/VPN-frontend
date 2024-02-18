f8abd1b7a0cc8f6f2c86cacc8eb6040d2412548c
Dmitry Kodolbenko <115988813+DmitryKodolbenko@users.noreply.github.com>
2024-02-18 19:36:34 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -36,21 +36,21 @@ declare global {
 
 const tonConnectUI = new TonConnectUI({ manifestUrl: 'https://72a879bd.manifests.pages.dev/devpn.txt' })
 
-// tonConnectUI.uiOptions = {
-//     walletsListConfiguration: {
-//         includeWallets: [
-//             {
-//                 appName: 'dewallet',
-//                 name: 'DeWallet',
-//                 imageUrl: 'https://avatars.githubusercontent.com/u/116884789?s=200&v=4',
-//                 aboutUrl: 'https://wallet.tg/',
-//                 universalLink: 'https://t.me/delabtonbot/wallet?attach=wallet', // https://t.me/delabtonbot/wallet 'https://v2.delabwallet.com/tonconnect' https://t.me/wallet?attach=wallet,
-//                 bridgeUrl: 'https://bridge.tonapi.io/bridge',
-//                 platforms: [ 'ios', 'android', 'macos', 'windows', 'linux' ]
-//             }
-//         ]
-//     }
-// }
+tonConnectUI.uiOptions = {
+    walletsListConfiguration: {
+        includeWallets: [
+            {
+                appName: 'dewallet',
+                name: 'DeWallet',
+                imageUrl: 'https://avatars.githubusercontent.com/u/116884789?s=200&v=4',
+                aboutUrl: 'https://wallet.tg/',
+                universalLink: 'https://t.me/delabtonbot/wallet?attach=wallet', // https://t.me/delabtonbot/wallet 'https://v2.delabwallet.com/tonconnect' https://t.me/wallet?attach=wallet,
+                bridgeUrl: 'https://bridge.tonapi.io/bridge',
+                platforms: [ 'ios', 'android', 'macos', 'windows', 'linux' ]
+            }
+        ]
+    }
+}
 
 WebAppSDK.ready()
 export const App: FC = () => {
