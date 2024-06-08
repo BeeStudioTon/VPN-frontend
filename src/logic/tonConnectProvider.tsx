20bb7aec5c8dd94481d7c3ae8cefa97fcb675690
anovic123 <anovicjosip@gmail.com>
2024-06-08 16:12:30 +0300

    fix: imageUrl  tonconnect

--- src/logic/tonConnectProvider.tsx
+++ src/logic/tonConnectProvider.tsx
@@ -1,41 +1,42 @@
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { TonConnectUIProvider } from 'delab-tonconnect-ui-react'
-import { WalletInfoBase, WalletInfoInjectable, WalletInfoRemote } from '@tonconnect/sdk'
+import { TonConnectUIProvider } from 'delab-tonconnect-ui-react';
+import { WalletInfoBase, WalletInfoInjectable, WalletInfoRemote } from '@tonconnect/sdk';
 
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode } from 'react';
 
 type UIWallet = WalletInfoBase &
-(Omit<WalletInfoInjectable, 'injected' | 'embedded'> | WalletInfoRemote)
+    (Omit<WalletInfoInjectable, 'injected' | 'embedded'> | WalletInfoRemote);
 
 type WalletsListConfiguration = {
     /**
      * Allows to include extra wallets to the wallets list in the modal.
      */
     includeWallets?: UIWallet[];
-}
+};
 
 interface TonConnectProviderProps {
     children: ReactNode;
 }
 
 export const TonConnectProvider: FC<TonConnectProviderProps> = ({ children }) => (
-    <TonConnectUIProvider manifestUrl="https://manifests.delab.team/devpn.txt"
-        walletsListConfiguration ={{
+    <TonConnectUIProvider
+        manifestUrl="https://manifests.delab.team/devpn.txt"
+        walletsListConfiguration={{
             includeWallets: [
                 {
                     appName: 'dewallet',
                     name: 'DeWallet',
-                    imageUrl: 'https://avatars.githubusercontent.com/u/116884789?s=200&v=4',
+                    imageUrl: 'https://de-cdn.delab.team/icons/DeLabLogo.png',
                     aboutUrl: 'https://wallet.tg/',
                     universalLink: 'https://t.me/delabtonbot/wallet?attach=wallet', // https://t.me/delabtonbot/wallet 'https://v2.delabwallet.com/tonconnect' https://t.me/wallet?attach=wallet,
                     bridgeUrl: 'https://bridge.tonapi.io/bridge',
-                    platforms: [ 'ios', 'android', 'macos', 'windows', 'linux' ]
-                }
-            ]
-        } }
+                    platforms: ['ios', 'android', 'macos', 'windows', 'linux'],
+                },
+            ],
+        }}
     >
         {children}
     </TonConnectUIProvider>
-)
+);
